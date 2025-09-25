870dd9295361bdb6a9e6937c99680d6418a50d5d
Aamir377300 <belalaamirkhan@gmail.com>
2025-09-26 00:45:40 +0530

    login and signup

---
+++ frontend/src/Welcome/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Welcome(){
+    return(
+        <div className="welcome-bar">
+            <p>Welcome</p>
+        </div>
+    )
+}
+
+export default Welcome
